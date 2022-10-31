50fcdf1966f77ce619f72beefb1ec4ab4425c7db
Minte Temple <mintetemple@gmail.com>
2022-10-31 14:24:02 -0700

    chore: update to test

--- Downloads/cbh-take-home-project/dpk.test.js
+++ Downloads/cbh-take-home-project/dpk.test.js
@@ -1,5 +1,6 @@
 const { deterministicPartitionKey, createHash, getCanditateFromEvent, convertToString } = require("./dpk");
 
+const longCandidate = '58540d4d440df8c6c6da0d79cfce715bc92953c6cde8be9f749790004ef2d5a7322d0fd5170eac9a37d57ee0cc975cfca068a60b01622529d9e0fd657f71b8e258540d4d440df8c6c6da0d79cfce715bc92953c6cde8be9f749790004ef2d5a7322d0fd5170eac9a37d57ee0cc975cfca068a60b01622529d9e0fd657f71b8e2'
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
@@ -13,6 +14,11 @@ describe("deterministicPartitionKey", () => {
   it("Returns the hash when given input but no partitionKey", () => {
     const trivialKey = deterministicPartitionKey({ partitionKey: null });
     const hashString = '58540d4d440df8c6c6da0d79cfce715bc92953c6cde8be9f749790004ef2d5a7322d0fd5170eac9a37d57ee0cc975cfca068a60b01622529d9e0fd657f71b8e2'
+    expect(trivialKey).toBe(hashString);
+  });
+   it("Returns a longer hash when a candidate is greate than MAX_PARTITION_KEY_LENGTH", () => {
+    const trivialKey = deterministicPartitionKey({ partitionKey: longCandidate });
+    const hashString = '58540d4d440df8c6c6da0d79cfce715bc92953c6cde8be9f749790004ef2d5a7322d0fd5170eac9a37d57ee0cc975cfca068a60b01622529d9e0fd657f71b8e258540d4d440df8c6c6da0d79cfce715bc92953c6cde8be9f749790004ef2d5a7322d0fd5170eac9a37d57ee0cc975cfca068a60b01622529d9e0fd657f71b8e2'
     expect(trivialKey).toBe(hashString);
   });
 });
